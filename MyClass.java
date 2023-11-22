d05951738c42e9895ae91876abf9b5c795a01418
domcaue <144695640+domcaue@users.noreply.github.com>
2023-11-22 12:10:56 -0300

    Add files via upload
    Trabalho feito com Fgutuzzo

---
+++ MyClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    String login = "senac@aluno.pr.senac.br";
+	    String senha = "senac@02";
+	    Scanner input = new Scanner (System.in);
+		System.out.println("Digite o Login");
+		String email = input.nextLine();
+		System.out.println("Digite a senha");
+		String senhaU = input.nextLine();
+		
+		if(email == login && senhaU == senha){
+		    System.out.println("Bem-vindo ao Sistema");
+		}else{
+		    System.out.println("Login e/ou senha errados");
+		}
+		
+	}
+}
